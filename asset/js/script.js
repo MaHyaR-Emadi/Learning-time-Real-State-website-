4354e9e59e7cbc00ca6f4ef8f83db1dc9dea8ff9
MaHyaR Emadi <emadi.mahyar@gmail.com>
2023-12-26 15:26:16 +0330

    feat : complete the property section

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,14 +1,13 @@
-`use strict` ;
-
+`use strict`;
 
 /**
- * navbar toggle in mobile 
+ * navbar toggle in mobile
  */
 
 const $navbar = document.querySelector("[data-navbar]");
-const $navtoggler =document.querySelector("[data-nav-toggler]");
+const $navtoggler = document.querySelector("[data-nav-toggler]");
 
-$navtoggler.addEventListener("click", () =>$navbar.classList.toggle("active"));
+$navtoggler.addEventListener("click", () => $navbar.classList.toggle("active"));
 
 /**
  * header scroll state
@@ -16,6 +15,16 @@ $navtoggler.addEventListener("click", () =>$navbar.classList.toggle("active"));
 
 const $header = document.querySelector("[data-header]");
 
-window.addEventListener("scroll", e => {
-    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+window.addEventListener("scroll", (e) => {
+   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+// add to favorite button toggle
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]") ;
+
+$toggleBtns.forEach($toggleBtns =>{
+   $toggleBtns.addEventListener("click", () => {
+      $toggleBtns.classList.toggle("active");
+   });
 });
